380fdc9b2d6c1b005a016d748ae818da6582b4b9
petrvanblokland <buro@petr.com>
2020-11-12 13:34:38 +0100

    working on automated site script with PageBotNano

--- PepperTom.py
+++ PepperTom.py
@@ -16,51 +16,36 @@
 #   Source builds the PepperTom.com website using PageBotNano the Website 
 #   publication class.
 #
-"""
-    >>> theme = BackToTheCity()
-    >>> title = randomTitle()
-    >>> author = randomName()
-    >>> siteName = 'pagebotnano_demo'
-    >>> pageData = parseMarkdownFile('../../TestPageContent.md')
-    >>> #pageData = parseMarkdownFile('../../PublishingVariables.md')
-    >>> templatePath = 'templated-hielo'
-    >>> templates = Templated(templatePath)
-    >>> website = Website(theme=theme, templates=templates)
-    >>> website.templates is templates
-    True
-    >>> website.compose(pageData)
-    >>> # Start MAMP to see this website on localhost, port 80
-    >>> website.export(website.MAMP_PATH + siteName)
-    >>> url = 'http:localhost:%d/%s' % (website.PORT or 80, siteName) 
-    >>> result = os.system(u'/usr/bin/open %s' % url)
-"""
-
-
 import os
+from PepperTomData import siteData
 from pagebotnano_060.toolbox.loremipsum import loremipsum, randomName, randomTitle
-from pagebotnano_060.templates.templated import Templated
+from pagebotnano_060.templates.templated import * # Import all templates classes.
 from pagebotnano_060.themes import BackToTheCity
 from pagebotnano_060.toolbox.markdown import parseMarkdownFile
 from pagebotnano_060.publications.website import Website
 
 PORT = 8888
 markdownPath = 'PepperTom.md'
-templatePath = 'templated-hielo'
 
 theme = BackToTheCity()
 
 siteName = 'peppertom'
 
 # Create a Website publications with this theme and templates
-templates = Templated(templatePath)
-print(templates.path)
+#templates = TemplatedBinary()
+#templates = TemplatedBroadcast()
+#templates = TemplatedCaminar()
+#templates = TemplatedFullmotion()
+templates = TemplatedFullmotion()
+#templates = TemplatedHielo()
+#templates = TemplatedInterphase()
+#templates = TemplatedIntrospect()
+#templates = TemplatedRoadtrip()
+
 website = Website(theme=theme, templates=templates, port=PORT)
 
-# Get dictionary if pages[pageData.id] = pageData
-pageData = parseMarkdownFile(markdownPath)
-
 # Compose the website with this content.
-website.compose(pageData)
+website.compose(siteData)
 
 # Start MAMP to see this website on localhost, port 80
 website.export(website.MAMP_PATH + siteName) 
--- PepperTomData.py
+++ PepperTomData.py
@@ -1,20 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#   P A G E B O T  N A N O
+#
+#   Copyright (c) 2020+ Buro Petr van Blokland + Claudia Mens
+#   www.pagebot.io
+#   Licensed under MIT conditions
+#
+#   Supporting DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
 
-from pagebotnano_060.toolbox.siteData import SiteData
+from pagebotnano_060.toolbox.sitedata import SiteData
+from pagebotnano_060.toolbox.dating import now
 
-site = SiteData('peppertom', 'Pepper+Tom')
-site.menuName = 'Menu'
-site.copyright = """<a href="https://peppertom.com">Pepper+Tom</a>, <a href="https://typetr.typenetwork.com">TYPETR</a>, <a href="https://designdesign.space">DesignDesign.Space</a>"""
-site.fontFamily = 'Upgrade'
-site.logo = """<span style="color:#ff0000;">|</span>d<span style="color: #EC842C;">|</span>"""
-site.logoFontFamily = 'PepperTom'
+siteData = SiteData('peppertom', 'Pepper+Tom')
+siteData.menuName = 'Menu'
+siteData.year = now().year
+siteData.copyright = ' | '.join((
+	'<a href="https://peppertom.com" target="external">Pepper+Tom</a>',
+	'<a href="https://typetr.typenetwork.com">TYPETR</a>',
+	'<a href="https://designdesign.space">DesignDesign.Space</a>'))
+siteData.fontFamily = 'Upgrade'
+siteData.logo = """<span style="color:#ff0000;">|</span>d<span style="color: #EC842C;">|</span>"""
+siteData.logoFontFamily = 'PepperTom'
+siteData.monoFontFamily = 'Courier New'
+siteData.iconFontFamily = 'FontAwesome'
+siteData.menuLinks = True # Force call to template._menuLinks()
 
 # Page index
 
-p = site.newPage('index', 'Home')
-p.template = 'index'
+p = siteData.newPage(id='index', title='Home', template='index')
 
 # Page index, banner
 
+p.bannerSlideShow = True
 p.bannerImage_1 = 'images/skirts/static1.squarespace.jpg'
 p.bannerTitle_1 = 'Laatste rokken'
 p.bannerSubtitle_1 = 'Er zijn er nog een paar'
@@ -45,6 +65,7 @@ p.bannerSubtitle_7 = 'My Banner 7@'
 
 # Page index, article 1
 
+p.imageArticles = True # Trigger the template._imageArticles call
 p.articleImage_1 = 'images/scarfs/groen1-1.jpg'
 p.articleSubhead_1 = 'Last ones in stock'
 p.articleHead_1 = 'Skirts'
@@ -65,7 +86,7 @@ p.articleFooter_1 = 'Footer of article 1'
 
 p.articleImage_2 = 'images/scarfs/goud1-1.jpg'
 p.articleSubhead_2 = 'Last ones in stock'
-p.articleHead = 'Scarves'
+p.articleHead_2 = 'Scarves'
 p.article_2 = """The scarves are the result of many peoples’ work and creativity from rural Bangladesh to the cities of the Netherlands and the shores of the United States. 
 Beautiful women of Bangladesh embroider the scarves by using the traditional Nakshi Kantha technique. The silk of the scarves is made of the best quality, Rajshahi Silk. The fabric is embossed in a workplace in Dhaka and embroidered in Dinajpur, a poor region in the northwest of Bangladesh. 
 
@@ -75,13 +96,16 @@ Under the name of Generous Gesture, the scarves have won a Bronze Award for the 
 Generous Gesture has been nominated for the German Design Award 2012. 
 Generous Gesture is a people project. Every piece we make is 100% fair trade. The principle op Generous Gesture is creating fair trade with sustainable products. Through an exchange of ideas and skills we create a win-win situation for all concerned parties. 
 """
+p.articleFooter_2 = 'Footer of article 2'
 
 # Page index, pullquote
 
+p.pullQuote = True # Trigger the template method
 p.pullQuoteImage_1 = 'images/notes/IMG_0929.jpeg'
 p.pullQuoteSubhead_1 = 'Pullquote subhead'
 p.pullQuoteHead_1 = 'Pullquote heading' 
 
+p.gallery = True # Trigger the template._gallery method call
 p.galleryHead = 'Gallery head'
 p.gallerySubhead = 'Gallery subhead'
 p.galleryImage_1 = 'images/notes/IMG_0926.jpeg'
@@ -95,110 +119,101 @@ p.galleryImage_6 = 'images/notes/IMG_0936.jpeg'
 
 # Page otherpage
 
-p = site.newPage('otherpage', 'Other Page', 'index')
+p = siteData.newPage('otherpage', 'Other Page', 'article')
+
+p.imageArticles = False # Ignore these in the template
+p.pullQuote = False
+
+p.bannerSlideShow = True
+p.bannerImage_1 = 'images/lookbook/IMG_2458.JPG'
+p.bannerTitle_1 = 'Laatste rokken'
+p.bannerSubtitle_1 = 'Er zijn er nog een paar'
+
+p.bannerImage_2 = 'images/lookbook/IMG_2361.JPG'
+p.bannerTitle_2 = 'Title 2'
+p.bannerSubtitle_2 = 'My Banner 2@'
+
+p.bannerImage_3 = 'images/lookbook/IMG_0966.jpg'
+p.bannerTitle_3 = 'Title 3'
+p.bannerSubtitle_3 = 'My Banner 3@'
+
+p.bannerImage_4 = 'images/lookbook/IMG_0861.jpg'
+p.bannerTitle_4 = 'Title 4'
+p.bannerSubtitle_4 = 'My Banner 4@'
+
+p.bannerImage_5 = 'images/lookbook/IMG_0860.jpg'
+p.bannerTitle_5 = 'Title 5'
+p.bannerSubtitle_5 = 'My Banner 5@'
+
+p.bannerImage_6 = 'images/lookbook/IMG_7719.jpg'
+p.bannerTitle_6 = 'Laatste rokken'
+p.bannerSubtitle_6 = 'Er zijn er nog een paar'
+
+p.gallery = True # Trigger the template._gallery method call
+p.galleryHead = 'Gallery head'
+p.gallerySubhead = 'Gallery subhead'
+p.galleryImage_1 = 'images/notes/IMG_0926.jpeg'
+p.galleryCaption_1 = """Caption of this image. A very long one. Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum."""
+p.galleryImage_2 = 'images/notes/IMG_0929.jpeg'
+p.galleryCaption_2 = 'Short caption of this image.'  
+p.galleryImage_3 = 'images/notes/IMG_0933.jpeg'
+p.galleryImage_4 = 'images/notes/IMG_0934.jpeg'
+p.galleryImage_5 = 'images/notes/IMG_0935.jpeg'
+p.galleryImage_6 = 'images/notes/IMG_0936.jpeg'
+
+# Page thirdpage
+
+p = siteData.newPage('thirdpage', 'Third page', 'generic')
 
 p.bannerImage_1 = 'images/lookbook/IMG_2458.JPG'
 p.bannerTitle_1 = 'Laatste rokken'
 p.bannerSubtitle_1 = 'Er zijn er nog een paar'
 
-.bannerImage 2. images/lookbook/IMG_2361.JPG
-.bannerTitle 2. Title 2
-.bannerSubtitle 2. My Banner 2@
+p.bannerImage_2 = 'images/lookbook/IMG_2361.JPG'
+p.bannerTitle_2 = 'Title 2'
+p.bannerSubtitle_2 = 'My Banner 2@'
 
-.bannerImage 3. images/lookbook/IMG_0966.jpg
-.bannerTitle 3. Title 3
-.bannerSubtitle 3. My Banner 3@
+p.bannerImage_3 = 'images/lookbook/IMG_0966.jpg'
+p.bannerTitle_3 = 'Title 3'
+p.bannerSubtitle_3 = 'My Banner 3@'
 
-.bannerImage 4. images/lookbook/IMG_0861.jpg
-.bannerTitle 4. Title 4
-.bannerSubtitle 4. My Banner 4@
+p.bannerImage_4 = 'images/lookbook/IMG_0861.jpg'
+p.bannerTitle_4 = 'Title 4'
+p.bannerSubtitle_4 = 'My Banner 4@'
 
-.bannerImage 5. images/lookbook/IMG_0860.jpg
-.bannerTitle 5. Title 5
-.bannerSubtitle 5. My Banner 5@
+p.bannerImage_5 = 'images/lookbook/IMG_0860.jpg'
+p.bannerTitle_5 = 'Title 5'
+p.bannerSubtitle_5 = 'My Banner 5@'
 
-.bannerImage 6. images/lookbook/IMG_7719.jpg
-.bannerTitle 6. Laatste rokken
-.bannerSubtitle 6. Er zijn er nog een paar
-
-.bannerImage 7. images/lookbook/IMG_8370.jpg
-.bannerTitle 7. Title 2
-.bannerSubtitle 7. My Banner 2@
-
-.bannerImage 8. images/lookbook/IMG_8437.jpg
-.bannerTitle 8. Title 3
-.bannerSubtitle 8. My Banner 3@
-
-.bannerImage 9. images/lookbook/IMG_8575.jpg
-.bannerTitle 9. Title 4
-.bannerSubtitle 9. My Banner 4@
-
-.bannerImage 10. images/lookbook/IMG_8601.jpg
-.bannerTitle 10. Title 5
-.bannerSubtitle 10. My Banner 5@
-
-.bannerImage 10. images/lookbook/IMG_8629.JPG
-.bannerTitle 10. Title 5
-.bannerSubtitle 10. My Banner 5@
-
-.bannerImage 11. images/lookbook/IMG_8857+2.jpg
-.bannerTitle 11. Title 5
-.bannerSubtitle 11. My Banner 5@
-
-.bannerImage 11. images/lookbook/IMG_9059.jpeg
-.bannerTitle 11. Title 5
-.bannerSubtitle 11. My Banner 5@
-
-.bannerImage 11. images/lookbook/IMG_9060.jpeg
-.bannerTitle 11. Title 5
-.bannerSubtitle 11. My Banner 5@
-
+p.bannerImage_6 = 'images/lookbook/IMG_7719.jpg'
+p.bannerTitle_6 = 'Laatste rokken'
+p.bannerSubtitle_6 = 'Er zijn er nog een paar'
 	
+# Page fourthpage
+
+p = siteData.newPage('fourthpage', 'Fourth page', 'elements')
+
+p.bannerImage_1 = 'images/lookbook/IMG_2458.JPG'
+p.bannerTitle_1 = 'Laatste rokken'
+p.bannerSubtitle_1 = 'Er zijn er nog een paar'
+
+p.bannerImage_2 = 'images/lookbook/IMG_2361.JPG'
+p.bannerTitle_2 = 'Title 2'
+p.bannerSubtitle_2 = 'My Banner 2@'
+
+p.bannerImage_3 = 'images/lookbook/IMG_0966.jpg'
+p.bannerTitle_3 = 'Title 3'
+p.bannerSubtitle_3 = 'My Banner 3@'
+
+p.bannerImage_4 = 'images/lookbook/IMG_0861.jpg'
+p.bannerTitle_4 = 'Title 4'
+p.bannerSubtitle_4 = 'My Banner 4@'
+
+p.bannerImage_5 = 'images/lookbook/IMG_0860.jpg'
+p.bannerTitle_5 = 'Title 5'
+p.bannerSubtitle_5 = 'My Banner 5@'
+
+p.bannerImage_6 = 'images/lookbook/IMG_7719.jpg'
+p.bannerTitle_6 = 'Laatste rokken'
+p.bannerSubtitle_6 = 'Er zijn er nog een paar'
 	
-.page thirdpage. Third Page
-.template generic.
-
-.bannerImage 1. images/lookbook/IMG_2458.JPG
-.bannerTitle 1. Laatste rokken
-.bannerSubtitle 1. Er zijn er nog een paar
-
-.bannerImage 2. images/lookbook/IMG_2361.JPG
-.bannerTitle 2. Title 2
-.bannerSubtitle 2. My Banner 2@
-
-.bannerImage 3. images/lookbook/IMG_0966.jpg
-.bannerTitle 3. Title 3
-.bannerSubtitle 3. My Banner 3@
-
-.bannerImage 4. images/lookbook/IMG_0861.jpg
-.bannerTitle 4. Title 4
-.bannerSubtitle 4. My Banner 4@
-
-.bannerImage 5. images/lookbook/IMG_0860.jpg
-.bannerTitle 5. Title 5
-.bannerSubtitle 5. My Banner 5@
-	
-	
-	
-.page fourthpage. Fourth Page
-.template elements.
-
-.bannerImage 1. images/lookbook/IMG_2458.JPG
-.bannerTitle 1. Laatste rokken
-.bannerSubtitle 1. Er zijn er nog een paar
-
-.bannerImage 2. images/lookbook/IMG_2361.JPG
-.bannerTitle 2. Title 2
-.bannerSubtitle 2. My Banner 2@
-
-.bannerImage 3. images/lookbook/IMG_0966.jpg
-.bannerTitle 3. Title 3
-.bannerSubtitle 3. My Banner 3@
-
-.bannerImage 4. images/lookbook/IMG_0861.jpg
-.bannerTitle 4. Title 4
-.bannerSubtitle 4. My Banner 4@
-
-.bannerImage 5. images/lookbook/IMG_0860.jpg
-.bannerTitle 5. Title 5
-.bannerSubtitle 5. My Banner 5@
